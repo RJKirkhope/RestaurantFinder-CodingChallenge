58f99745f36e9d0182d9ef222979591103091619
Rjkirkhope <rjkirkhope@gmail.com>
2020-11-22 17:57:42 -0700

    Pagination and search working together, a little bit of tidying up as well, still need to do some heavy styling including @media for different screensizes, as well as filter logic. WIP

--- src/App.js
+++ src/App.js
@@ -2,14 +2,13 @@ import React, { Component } from 'react';
 import './App.css';
 import Header from './Header.js';
 import Footer from './Footer.js';
-import Search from './Search.js';
 import Table from './Table.js';
 
 class App extends Component {
   state={
     error:null,
     isLoaded:false,
-    restaurantData:[],
+    data:[],
   }
   
   componentDidMount(){
@@ -23,8 +22,7 @@ class App extends Component {
         (res) => {
           this.setState({
             isLoaded: true,
-            restaurantData: res,
-            search: "",
+            data: res,
           });
         },
         (error) => {
@@ -35,18 +33,9 @@ class App extends Component {
         }
       )
         }
-    receiveSearch = (event) => {
-      this.setState({search: event})
-    }
 
   render() {
-  const {error, isLoaded, restaurantData} = this.state;
-
-  let searchableRestaurants = restaurantData.filter(
-    (restaurant) => {
-      return restaurant.name.toLowerCase().indexOf(this.state.search.toLowerCase()) !== -1;
-    }
-  );
+  const {error, isLoaded, data} = this.state;
 
   if (error) {
     return <div>Error: {error.message}</div>;
@@ -57,17 +46,8 @@ class App extends Component {
     return (
       <div className="App">
         <Header />
-        <div className="restaurantSearch">
-            <span className="search-text">
-              <h4>Search For A Restaurant</h4>
-            </span>
-            <Search receiveSearch={this.receiveSearch}/>
-        </div>
         <div className="restaurantTable">
-            <span className="table-text">
-              <h4>Here Are The Restaurants</h4>
-            </span>
-            <Table receiveTable={searchableRestaurants}/>
+            <Table receiveTable={data}/>
         </div>
         <Footer />
       </div>
--- src/Table.js
+++ src/Table.js
@@ -1,10 +1,57 @@
 import React, {Component} from 'react';
+import Search from './Search.js';
 import './App.css';
 
 class Table extends Component {
+  
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: this.props.receiveTable,
+      currentPage: 1,
+      itemsPerPage: 10,
+      search: "",
+    };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(event) {
+    this.setState({
+      currentPage: Number(event.target.id)
+    });
+  }
+
+  receiveSearch = (event) => {
+    this.setState({search: event})
+  }
 
   render(){
-    var listItems = this.props.receiveTable.map(function(restaurant, i) {
+    const { data, currentPage, itemsPerPage } = this.state;
+    
+    let searchableData = data.filter(
+      (restaurant) => {
+        return restaurant.name.toLowerCase().indexOf(this.state.search.toLowerCase()) !== -1;
+      }
+    );
+
+    const indexOfLastItem = currentPage * itemsPerPage;
+    const indexOfFirstItem = indexOfLastItem - itemsPerPage;
+    const currentData = searchableData.slice(indexOfFirstItem, indexOfLastItem);
+    const pageNumbers = [];
+
+    for (let i = 1; i <= Math.ceil(searchableData.length / itemsPerPage); i++) {
+      pageNumbers.push(i);
+    }
+
+    const renderPageNumbers = pageNumbers.map(number => {
+      return (
+        <button className='pageButton' key={number} id={number} onClick={this.handleClick}>
+          {number}
+        </button>
+      );
+    });
+
+    var listItems = currentData.map(function(restaurant, i) {
 
     return(
           <tr key={i}>
@@ -13,24 +60,40 @@ class Table extends Component {
               <td style={{whiteSpace: 'pre-wrap'}}>{restaurant['genre'].split(',').join('\n')}</td>
               <td style={{whiteSpace: 'pre-wrap'}}>{restaurant['hours'].split(';').join('\n')}</td>
               <td>{restaurant['telephone']}</td>
-              <td><button><a href={restaurant['website']} target="_blank">Visit Website</a></button></td>
+              <td><button className='websiteTd'><a href={restaurant['website']} target="_blank">Visit Website</a></button></td>
           </tr>
       )
-  });
+    });
 
     return (
       <div className="tableDiv">
-        <table>
-          <tr>
-            <th className='name'>Name</th>
-            <th className='address'>Address</th>
-            <th className='genre'>Genre</th>
-            <th className='hours'>Hours</th>
-            <th className='phone'>Phone</th>
-            <th className='websites'>Website</th>
-          </tr>
-        {listItems}
-        </table>
+        <div className="restaurantSearch">
+          <span className="search-text">
+            <h4>Search For A Restaurant</h4>
+          </span>
+          <Search receiveSearch={this.receiveSearch}/>
+          </div>
+            <div className="pageButtonContainer">
+              <div className='pageIndicator'>Current Page: {currentPage}</div>
+              Page:{renderPageNumbers}
+            </div>
+          <table>
+            <tbody>
+              <tr>
+                <th className='name'>Name</th>
+                <th className='address'>Address</th>
+                <th className='genre'>Genre</th>
+                <th className='hours'>Hours</th>
+                <th className='phone'>Phone</th>
+                <th className='website'>Website</th>
+              </tr>
+              {listItems}
+            </tbody>
+          </table>
+            <div className="pageButtonContainer">
+              Page: {renderPageNumbers}
+              <div className='pageIndicator'>Current Page: {currentPage}</div>
+            </div>
       </div>
     )
   }
